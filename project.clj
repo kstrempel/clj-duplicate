d2c817fe1d05173bb07b21ba8f31f8f2ede03457
Kai Strempel <kstrempel@gmail.com>
2014-04-05 08:03:54 +0200

    first commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject clj_duplicate "0.8.0"
+  :description "Find duplicates of pictures"
+  :url "http://str3.me/clj_duplicate"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
