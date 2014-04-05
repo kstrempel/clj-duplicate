cea260ecba028d29d0dd7cfa7afddd0caba84425
Kai Strempel <kstrempel@gmail.com>
2014-04-05 17:09:11 +0200

    now with first failed difference

--- test/clj_duplicate/core_test.clj
+++ test/clj_duplicate/core_test.clj
@@ -4,6 +4,8 @@
 
 (def picture_a "cat_grumpy_orig.png")
 (def picture_b "cat_grumpy_modif.png")
+(def picture_contact "contact.png")
+(def picture_superman "superman.png")
 
 (defn compare_pictures [a b]
   (let [hasha (generate-dhash a)
@@ -24,3 +26,8 @@
   (testing "Pictures with small difference"
     (let [hashes (compare_pictures picture_a picture_b)]
     (is (apply = hashes)))))
+
+(deftest compare-google-drive-soundcloud-pictures
+  (testing "Pictures with big difference"
+    (let [hashes (compare_pictures picture_contact picture_superman)]
+    (is (not (apply = hashes))))))
